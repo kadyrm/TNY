94647ff58b91585c7f18a0be17ffe1bd92f09380
admin <thenomadyurts@gmail.com>
2020-02-13 10:04:12 +0600

    [VP] Activated VersionPress 4.0-beta2
    VP-Action: versionpress/activate/4.0-beta2

--- wp-config.php
+++ wp-config.php
@@ -84,6 +84,7 @@ $table_prefix = 'wp_';
 define( 'WP_DEBUG', false );
 
 define('VP_GIT_BINARY', '/usr/local/bin/git');
+define('VP_ENVIRONMENT', 'aybek');
 /* That's all, stop editing! Happy publishing. */
 
 /** Absolute path to the WordPress directory. */
